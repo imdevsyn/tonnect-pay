8f1349e9a4a2cac37be3760c72211d591e78d0dd
imdevsyn <imdevsyn@gmail.com>
2025-07-07 18:27:06 -0300

    chore: initial commit for tonnect-pay

---
+++ client/app/mini-app/page.tsx
@@ -0,0 +1,10 @@
+
+export default function MiniApp() {
+  return (
+    <div className="flex justify-center items-center min-h-screen bg-gray-400">
+      <div className="w-full max-w-sm bg-amber-300 rounded-lg p-4">
+        test
+      </div>
+    </div>
+  )
+}
