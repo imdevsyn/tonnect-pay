52568ba80fbd6449249f39b29039950d15e8b6f0
imdevsyn <imdevsyn@gmail.com>
2025-07-14 22:12:07 -0300

    fix: adjust app url

--- client/app/providers.tsx
+++ client/app/providers.tsx
@@ -4,7 +4,7 @@ import { TonConnectUIProvider } from "@tonconnect/ui-react";
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <TonConnectUIProvider manifestUrl="https://tonect-pay.vercel.app/tonconnect-manifest.json">
+    <TonConnectUIProvider manifestUrl="https://tonnect-pay.vercel.app/tonconnect-manifest.json">
       {children}
     </TonConnectUIProvider>
   );
