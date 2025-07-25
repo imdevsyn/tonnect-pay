61f96cb5986a4e742629566ff65243f60a4507df
imdevsyn <imdevsyn@gmail.com>
2025-07-24 23:07:13 -0300

    feat: add history sketch for future development

--- client/app/mini-app/(tabs)/history/page.tsx
+++ client/app/mini-app/(tabs)/history/page.tsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React from "react";
 
 export default function History() {
   return (
-    <div>History</div>
-  )
+    <div className="flex h-full items-center justify-center">
+      <div className="text-xs text-gray-500 text-center mt-4 p-3 bg-yellow-50 rounded-lg border border-yellow-200 w-52 mx-auto">
+        ⚠️ Histórico de transferências em desenvolvimento.
+      </div>
+    </div>
+  );
 }
